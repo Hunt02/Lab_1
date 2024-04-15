1f209771e077e7237a699b8812168e0d07eed0b1
noname <tranhungtien@TranHungTien.local>
2024-04-15 14:56:36 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import * as react from 'react';
+import Calculators from './Component/Calculators'
+const App = () =>{
+  return(
+    //<Project1/>
+    //<Project2/>
+    //<Project3/>
+   //<Project4/>
+    //<Project5/>
+    //<Project6/>
+   //<Project7/>
+    //<Project8/>
+    <Calculators/>
+  );
+};
+export default App;
